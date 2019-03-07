aba4ff9a4236047295fd7efa3facc3f9b3048a0d
Gabriela Flores <gabiiflores09@gmail.com>
2019-03-07 11:39:09 -0300

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Login1Component } from './login1/login1.component';
+import { Login2Component } from './login2/login2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Login1Component,
+    Login2Component
   ],
   imports: [
     BrowserModule
